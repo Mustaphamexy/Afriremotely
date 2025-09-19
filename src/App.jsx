ab4edf324d15ba54dd015f91bb81bc32417a93a3
Mustaphamexy <mustaphamexy111@gmail.com>
2025-09-19 12:19:54 +0100

    Remove ghost Signup.jsx and fix case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import JobsPage from './pages/JobsPage';
-import SignUp from "./pages/Signup"
+import SignUp from "./pages/SignUp"
 import Login from "./pages/Login"
 import About from "./pages/About"
 import Contact from "./pages/ContactPage"
